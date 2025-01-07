1fc5f1af7a509434159dfa99fbe73357be5e8922
Maboku1111 <mabokuseimela6@gmail.com>
2025-01-07 10:01:29 +0200

    step 1 message digest created

--- src/Main.java
+++ src/Main.java
@@ -1,5 +1,24 @@
+import java.security.MessageDigest;
+import java.security.NoSuchAlgorithmException;
+
 public class Main {
-    public static void main(String[] args) {
-        System.out.println("Hello, World!");
+    public static void main(String[] args) throws NoSuchAlgorithmException {
+        byte[] password = "123456".getBytes();
+
+        MessageDigest md = MessageDigest.getInstance("MD5");
+
+        md.update(password);
+
+        byte[] digest = md.digest();
+
+        System.out.println("digest: " + bytesToHex(digest));
+    }
+
+    public static String bytesToHex(byte[] bytes) {
+        StringBuilder out = new StringBuilder();
+        for (byte b : bytes) {
+            out.append(String.format("%02X", b));
+        }
+        return out.toString();
     }
 }
